62851659cac3710f9bb785a5668328597fa1ec0b
sanskrutipatro <patroswastik.jsdev@gmail.com>
2024-10-27 02:39:22 +0530

    feat: user can create free hand drawings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,154 @@
-import Image from "next/image";
+"use client";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useEffect, useRef, useState, MouseEvent } from "react";
+
+type TPoint = {
+  x: number;
+  y: number;
+};
+
+const AVAILABLE_SHAPES = ["path", "rect"] as const;
+
+type TShape = (typeof AVAILABLE_SHAPES)[number];
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [shapes, setShapes] = useState<TPoint[][]>([]);
+  const [activePath, setActivePath] = useState<TPoint[]>([]);
+  const [activeShapeType, setActiveShapeType] = useState<TShape>(
+    AVAILABLE_SHAPES[0]
+  );
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const handleMouseDown = ({
+    clientX,
+    clientY,
+  }: MouseEvent<HTMLCanvasElement>) => {
+    setIsDrawing(true);
+    const newPoint = { x: clientX, y: clientY };
+    setActivePath([newPoint]);
+  };
+
+  const handleMouseMove = ({
+    clientX,
+    clientY,
+  }: MouseEvent<HTMLCanvasElement>) => {
+    if (!isDrawing) return;
+
+    const newPoint = { x: clientX, y: clientY };
+    setActivePath([...activePath, newPoint]);
+  };
+
+  const handleMouseUp = () => {
+    if (!isDrawing) return;
+
+    setShapes(shapes.length > 0 ? [...shapes, activePath] : [activePath]);
+    setActivePath([]);
+    setIsDrawing(false);
+  };
+
+  // Initialize canvas dimensions
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const resizeCanvas = () => {
+      const rect = canvas.getBoundingClientRect();
+      canvas.width = rect.width;
+      canvas.height = rect.height;
+    };
+
+    resizeCanvas();
+    window.addEventListener("resize", resizeCanvas);
+
+    return () => window.removeEventListener("resize", resizeCanvas);
+  }, []);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+
+    // Set drawing styles
+    ctx.strokeStyle = "#000";
+    ctx.lineWidth = 2;
+
+    ctx.fillStyle = "gray";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    const whiteStaging = {
+      width: canvas.width / 2,
+      height: canvas.height / 2,
+    };
+
+    ctx.fillStyle = "#fff";
+    ctx.fillRect(
+      whiteStaging.width / 2,
+      whiteStaging.height / 2,
+      whiteStaging.width,
+      whiteStaging.height
+    );
+
+    // Draw all completed shapes
+    const allShapes = [...shapes, activePath];
+    allShapes.forEach((shape) => {
+      if (shape.length < 1) return;
+
+      ctx.beginPath();
+      ctx.moveTo(shape[0].x, shape[0].y);
+      shape.forEach((point) => {
+        ctx.lineTo(point.x, point.y);
+      });
+      ctx.stroke();
+    });
+
+    return () => ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }, [shapes, activePath]);
+  return (
+    <div className="relative font-[family-name:var(--font-geist-sans)] h-screen">
+      <canvas
+        ref={canvasRef}
+        className="w-full h-full"
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseUp}
+      />
+
+      <div className="bg-white p-2 absolute top-0 left-0 w-fit h-fit">
+        <div className="flex gap-2">
+          <Select
+            onValueChange={(selectedShapeType: TShape) =>
+              setActiveShapeType(selectedShapeType)
+            }
+            defaultValue={activeShapeType}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <SelectTrigger className="capitalize">
+              <SelectValue placeholder="Select Shape" />
+            </SelectTrigger>
+            <SelectContent>
+              {AVAILABLE_SHAPES.map((singleShapeType) => (
+                <SelectItem
+                  className="capitalize"
+                  key={singleShapeType}
+                  value={singleShapeType}
+                >
+                  {singleShapeType}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
